98c59215ce01517cdaa335b7a59946227aca2e35
Akshat Vishwakarma <akvs0707@gmail.com>
2022-11-07 22:06:30 +0530

    feat: step 2 game

--- src/components/GameHand/index.js
+++ src/components/GameHand/index.js
@@ -1,13 +1,22 @@
 import "./style.scss";
 
-const GameHand = ({ icon, handType }) => {
+const GameHand = ({ icon, handType, size }) => {
   const gameHandWrapperClass = () => {
     switch (handType) {
       case "rock":
+        if (size === "2x") {
+          return "game-hand-wrapper rock-hand size-large";
+        }
         return "game-hand-wrapper rock-hand";
       case "paper":
+        if (size === "2x") {
+          return "game-hand-wrapper paper-hand size-large";
+        }
         return "game-hand-wrapper paper-hand";
       case "scissor":
+        if (size === "2x") {
+          return "game-hand-wrapper scissor-hand size-large";
+        }
         return "game-hand-wrapper scissor-hand";
       default:
         break;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,95 @@
+import { useState, useEffect } from "react";
 import Header from "components/Header";
 import GameHand from "components/GameHand";
 
 import PaperIcon from "images/icon-paper.svg";
 import RockIcon from "images/icon-rock.svg";
 import ScissorIcon from "images/icon-scissors.svg";
+import TriangleBG from "images/bg-triangle.svg";
 
 import "./style.scss";
 
+const STEP_1 = "step 1";
+const STEP_2 = "step 2";
+
 const Home = () => {
+  const [gameStep, setGameStep] = useState(STEP_1);
+  const [userClickedHandType, setUserClickedHandType] = useState(null);
+
+  const handleHandBtnClick = (hand) => {
+    setGameStep(STEP_2);
+    setUserClickedHandType(hand);
+  };
+
+  const renderGameStep = () => {
+    if (gameStep === STEP_1) {
+      return gameStep1;
+    }
+    if (gameStep === STEP_2) {
+      return gameStep2;
+    }
+    return null;
+  };
+
+  const gameStep1 = (
+    <div className="game-area">
+      <img src={TriangleBG} alt="triangle bg" className="triangle-bg" />
+      <div className="game-line-1">
+        <button
+          onClick={() => handleHandBtnClick("paper")}
+          className="game-hand-btn"
+        >
+          <GameHand handType="paper" icon={PaperIcon} />
+        </button>
+        <button
+          onClick={() => handleHandBtnClick("scissor")}
+          className="game-hand-btn"
+        >
+          <GameHand handType="scissor" icon={ScissorIcon} />
+        </button>
+      </div>
+      <div className="game-line-2">
+        <button
+          onClick={() => handleHandBtnClick("rock")}
+          className="game-hand-btn"
+        >
+          <GameHand handType="rock" icon={RockIcon} />
+        </button>
+      </div>
+    </div>
+  );
+
+  const getHandIcon = () => {
+    if (userClickedHandType === "rock") {
+      return RockIcon;
+    }
+    if (userClickedHandType === "paper") {
+      return PaperIcon;
+    }
+    return ScissorIcon;
+  };
+
+  const gameStep2 = (
+    <div className="game-area_step_2">
+      <div className="hand-picked-wrapper">
+        <h2 className="heading">You picked</h2>
+        <GameHand
+          size="2x"
+          handType={userClickedHandType}
+          icon={getHandIcon()}
+        />
+      </div>
+      <div className="hand-picked-wrapper">
+        <h2 className="heading">the house picked</h2>
+        <div className="transparent-block" />
+      </div>
+    </div>
+  );
+
   return (
     <div className="home">
       <Header />
-      <GameHand handType="paper" icon={PaperIcon} />
-      <GameHand handType="rock" icon={RockIcon} />
-      <GameHand handType="scissor" icon={ScissorIcon} />
+      {renderGameStep()}
     </div>
   );
 };
